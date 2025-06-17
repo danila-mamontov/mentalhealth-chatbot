14be426e654e965a6cb28122d7bc2947d54adcac
Danila Mamontov <goomenny@gmail.com>
2025-06-17 14:03:48 +0200

    localization file added

---
+++ localization.py
@@ -0,0 +1,282 @@
+translations = {
+    'open_profile_button': {
+        'en': 'Settings',
+        'de': 'Einstellungen',
+        'ru': 'Настройки'
+    },
+    'profile_template': {
+        'en': '<b>Your profile:</b>\n'
+              'User ID: {user_id}\n'
+              'Language: {language}\n'
+              'Gender: {gender}\n'
+              'Age: {age}\n',
+
+        'de': '<b>Ihr Profil:</b>\n'
+              'Benutzer-ID: {user_id}\n'  
+              'Sprache: {language}\n'
+              'Geschlecht: {gender}\n'
+              'Alter: {age}\n',
+        'ru': '<b>Ваш профиль:</b>\n'
+              'ID пользователя: {user_id}\n'
+              'Язык: {language}\n'
+              'Пол: {gender}\n'
+              'Возраст: {age}\n'
+    },
+
+    'change_language_button': {
+        'en': 'Change Language',
+        'de': 'Sprache ändern',
+        'ru': 'Изменить язык'
+    },
+    'change_age_button': {
+        'en': 'Change Age',
+        'de': 'Alter ändern',
+        'ru': 'Изменить возраст'
+    },
+    'change_gender_button': {
+        'en': 'Change Gender',
+        'de': 'Geschlecht ändern',
+        'ru': 'Изменить пол'
+    },
+
+
+
+    'website': {
+        'en': 'Our Website',
+        'de': 'Unsere Webseite',
+        'ru': 'Наш сайт'
+    },
+    'help': {
+        'en': 'Here are all available commands:\n/start - Start the bot\n/help - Get help\n/support - Contact the support team\nIf you need further assistance, please contact the support team.',
+        'de': 'Hier sind alle verfügbaren Befehle:\n/start - Starten Sie den Bot\n/help - Hilfe erhalten\n/support - Kontaktieren Sie das Support-Team\nWenn Sie weitere Unterstützung benötigen, wenden Sie sich bitte an das Support-Team.',
+        'ru': 'Вот все доступные команды:\n/start - Начать бота\n/help - Получить помощь\n/support - Связаться с командой поддержки\nЕсли вам нужна дополнительная помощь, обратитесь в службу поддержки.'
+    },
+    'yes': {
+        'en': 'Yes',
+        'de': 'Ja',
+        'ru': 'Да'
+    },
+    'no': {
+        'en': 'No',
+        'de': 'Nein',
+        'ru': 'Нет'
+    },
+    'noanswer': {
+        'en': 'No answer',
+        'de': 'Keine Antwort',
+        'ru': 'Без ответа'
+    },
+    'back': {
+        'en': 'Back',
+        'de': 'Zurück',
+        'ru': 'Назад'
+    },
+    'next': {
+        'en': 'Next',
+        'de': 'Weiter',
+        'ru': 'Далее'
+    },
+    'finish_button': {
+        'en': 'Finish the survey',
+        'de': 'Umfrage beenden',
+        'ru': 'Завершить опрос'
+    },
+
+    'language_selection': {
+        'en': 'Please select your language:',
+        'de': 'Bitte wählen Sie Ihre Sprache:',
+        'ru': 'Пожалуйста, выберите Ваш язык:'
+    },
+    'consent_message': {
+        'en': 'All data collected will be anonymized and used for research purposes only. Do you consent to participate in the study?',
+        'de': 'Alle gesammelten Daten werden anonymisiert und ausschließlich zu Forschungszwecken verwendet. Stimmen Sie der Teilnahme an der Studie zu?',
+        'ru': 'Все собранные данные будут анонимизированы и использованы исключительно в научных целях. Вы согласны участвовать в исследовании?'
+    },
+    'consent_yes': {
+        'en': 'Thank you for your consent!',
+        'de': 'Vielen Dank für Ihre Zustimmung!',
+        'ru': 'Спасибо за ваше согласие!'
+    },
+    'consent_no': {
+        'en': 'If you do not consent, you cannot participate in the study. If you change your mind, you can /start the survey again.',
+        'de': 'Wenn Sie nicht zustimmen, können Sie nicht an der Studie teilnehmen. Wenn Sie Ihre Meinung ändern, können Sie den Fragebogen erneut starten (/start).',
+        'ru': 'Если вы не согласны, вы не можете участвовать в исследовании. Если вы передумаете, вы можете начать опрос заново /start.'
+    },
+    'gender_selection': {
+        'en': 'Please select your gender:',
+        'de': 'Bitte wählen Sie Ihr Geschlecht:',
+        'ru': 'Пожалуйста, выберите ваш пол:'
+    },
+    'male': {
+        'en': 'Male',
+        'de': 'Männlich',
+        'ru': 'Мужской'
+    },
+    'female': {
+        'en': 'Female',
+        'de': 'Weiblich',
+        'ru': 'Женский'
+    },
+    'age_selection': {
+        'en': 'Please choose your age:',
+        'de': 'Bitte wählen Sie Ihr Alter:',
+        'ru': 'Пожалуйста, выберите ваш возраст:'
+    },
+    'depression_diagnosis': {
+        'en': 'Have you ever been diagnosed with a depressive disorder?',
+        'de': 'Wurde bei Ihnen jemals eine depressive Störung diagnostiziert?',
+        'ru': 'Было ли у вас когда-либо диагностировано депрессивное расстройство?'
+    },
+    'depressive_feelings': {
+        'en': 'Do you feel that you have had/have depression?',
+        'de': 'Haben Sie das Gefühl, dass Sie eine Depression hatten/haben?',
+        'ru': 'Считаете ли Вы, что у Вас была/есть депрессия?'
+    },
+    'treatment_selection': {
+        'en': 'Do you take antidepressants currently or lately?',
+        'de': 'Nehmen Sie derzeit oder in letzter Zeit Antidepressiva ein?',
+        'ru': 'Принимаете ли Вы в настоящее время/прошлом лекарства от депрессии?'
+    },
+    'welcome_message': {
+        'en': "👋 Welcome!\n\n"
+                         "This bot is part of a <b>scientific study</b> aimed at collecting data on people's psychophysiological states.\n\n"
+                         "⚠️ <b>Please note:</b> This bot <b>does not provide psychological support or counseling.</b>\n\n"
+                         "✅ Participation is <b>completely voluntary</b>, and you can stop at any time by typing.\n\n"
+                         "🙏 Thank you for contributing to science!",
+        'de': "👋 Willkommen!\n\n"
+                         "Dieser Bot ist Teil einer <b>wissenschaftlichen Studie</b>, die darauf abzielt, Daten über den psychophysiologischen Zustand von Menschen zu sammeln.\n\n"
+                         "⚠️ <b>Bitte beachten Sie:</b> Dieser Bot <b>bietet keine psychologische Unterstützung oder Beratung.</b>\n\n"
+                         "✅ Die Teilnahme ist <b>völlig freiwillig</b>, und Sie können jederzeit abbrechen.\n\n"
+                         "🙏 Vielen Dank für Ihren Beitrag zur Wissenschaft!",
+        'ru': "👋 Здравствуйте!\n\n"
+                         "Этот бот является частью <b>научного исследования</b>, целью которого является сбор данных о психофизиологическом состояний людей.\n\n"
+                         "⚠️ <b>Важно:</b> Этот бот <b>не оказывает психологической помощи и не заменяет консультацию специалиста.</b>\n\n"
+                         "✅ Ваше участие <b>полностью добровольное</b>, и вы можете прекратить его в любой момент.\n\n"
+                         "🙏 Спасибо за ваш вклад в науку!",
+    },
+
+    "info_message": {
+        "en": "❗ If at any point you feel discomfort, distress, or need support, we strongly recommend reaching out to professionals:\n"
+                         "📞 <b>Support hotlines:</b> (provide relevant contacts)\n"
+                         "🏥 <b>Professional help:</b> If you have concerns about your emotional well-being, consider consulting a psychologist or therapist.\n"
+                         "👨‍👩‍👧‍👦 <b>Talk to close ones:</b> Sharing your feelings with trusted friends or family can be helpful.\n\n",
+        "de":            "❗ Falls Sie sich während der Nutzung unwohl fühlen oder Unterstützung benötigen, empfehlen wir Ihnen, sich an professionelle Stellen zu wenden:\n"
+                         "📞 <b>Krisentelefone:</b> (relevante Kontakte angeben)\n"
+                         "🏥 <b>Professionelle Hilfe:</b> Wenn Sie sich Sorgen um Ihr emotionales Wohlbefinden machen, ziehen Sie eine Beratung durch einen Psychologen oder Therapeuten in Betracht.\n"
+                         "👨‍👩‍👧‍👦 <b>Sprechen Sie mit Vertrauenspersonen:</b> Der Austausch mit Freunden oder Familie kann hilfreich sein.\n\n",
+        "ru":            "❗ Если в процессе взаимодействия вы почувствуете дискомфорт или вам потребуется поддержка, рекомендуем обратиться к профессионалам:\n"
+                         "📞 <b>Телефоны доверия:</b> (укажите актуальные контакты)\n"
+                         "🏥 <b>Профессиональная помощь:</b> Если у вас есть вопросы касательно вашего эмоционального состояния, обратитесь к психологу или психотерапевту.\n"
+                         "👨‍👩‍👧‍👦 <b>Поговорите с близкими:</b> Обсуждение своих чувств с людьми, которым вы доверяете, может помочь.\n\n"
+    },
+    
+    'main_menu_message': {
+        'en': '<b>Мenu</b>\n'
+              'Please choose an option:',
+        'de': '<b>Мenü</b>\n'
+            'Bitte wählen Sie eine Option:',
+        'ru': '<b>Меню</b>\n'
+            'Пожалуйста, выберите опцию:'
+    },
+    'phq9_survey_button': {
+        'en': 'Start the Survey',
+        'de': 'Die Umfrage Starten',
+        'ru': 'Начать Опрос'
+    },
+    'main_survey_button': {
+        'en': 'Start Main Survey',
+        'de': 'Hauptumfrage starten',
+        'ru': 'Начать основной опрос'
+    },
+
+    'intro_phq9_message': {
+        'en':
+
+"""📋 Survey Instructions
+This survey aims to understand your emotional state and mental health level <b>over the past two weeks</b>.
+📊 Survey Structure:
+•  Total of 22 questions, divided into two parts
+•  Part 1: 8 multiple-choice questions
+•  Part 2: 14 open-ended <b>voice response questions</b>
+•  Estimated time: 15-20 minutes
+💡 Participation Guidelines:
+•  Please answer based on your true feelings, there are no right or wrong answers
+⚠️ This survey does not provide psychological diagnosis or treatment services""",
+
+        'de': '📝 In dieser Umfrage werden Sie nach Ihrer Stimmung, Ihren Gedanken und Gefühlen gefragt.',
+        'ru': '📝 В этом опросе вам будут заданы вопросы о вашем настроении, мыслях и чувствах.'
+    },
+    'starting_phq9': {
+        'en' : "Over <u>the last 2 weeks</u>, how often have you been bothered by any of the following problems?",
+        'de' : "Wie oft haben Sie in <u>den letzten 2 Wochen</u> unter den folgenden Problemen gelitten?",
+        'ru' : "Как часто за <u>последние 2 недели</u> вас беспокоили следующие проблемы?"
+    },
+    'end_phq9_message': {
+        'en': 'Thank you for participating in the survey!',
+        'de': 'Vielen Dank für die Teilnahme an der Umfrage!',
+        'ru': 'Спасибо за участие в опросе!'
+    },
+
+    'intro_main_message': {
+        'en': "📝 In this survey, we will ask you a series of questions about your thoughts, feelings, and experiences. "
+                                 "These questions will help us better understand emotional well-being and how people experience their daily lives.\n\n"
+                                 "💡 <b>What to expect?</b>\n"
+                                 "  -<b>14 questions</b> about your emotions, mood, and interactions with others.\n"
+                                 "  -You need to respond to questions by <i><b>recording voice messages</b></i>.\n"
+                                 "  -There are no right or wrong answers—just share what feels true to you.\n"
+                                 "  -Your responses are <b>confidential</b> and used only for scientific research.\n\n"
+                                 "⚠️ <b>Important:</b> This bot <b>does not provide psychological help</b>. If you feel uncomfortable, you can stop at any time. "
+                                 "If you need support, please reach out to a professional.\n\n"
+                                 "👉 When you're ready, send any voice message to start answering the questions!",
+        'de': "📝 In dieser Umfrage werden wir Ihnen eine Reihe von Fragen zu Ihren Gedanken, Gefühlen und Erfahrungen stellen. "
+                                "Diese Fragen helfen uns, das emotionale Wohlbefinden und die täglichen Erfahrungen der Menschen besser zu verstehen.\n\n"
+                                "💡 <b>Was Sie erwartet:</b>\n"
+                                "  -<b>14 Fragen</b> zu Ihren Emotionen, Ihrer Stimmung und Ihren Interaktionen mit anderen.\n"
+                                "  -Sie sollten auf Fragen mit <i><b>Sprachnachrichten</b></i> antworten.\n"
+                                "  -Es gibt keine richtigen oder falschen Antworten – teilen Sie einfach, was für Sie wahr ist.\n"
+                                "  -Ihre Antworten sind <b>vertraulich</b> und werden nur für wissenschaftliche Forschungszwecke verwendet.\n\n"
+                                "⚠️ <b>Wichtig:</b> Dieser Bot <b>bietet keine psychologische Hilfe</b>. Wenn Sie sich unwohl fühlen, können Sie jederzeit abbrechen. "
+                                "Wenn Sie Unterstützung benötigen, wenden Sie sich bitte an einen Fachmann.\n\n"
+                                "👉 Wenn Sie bereit sind, senden Sie eine Sprachnachricht, um mit den Fragen zu beginnen!",
+        'ru' : "📝 В этом опросе мы зададим вам ряд вопросов о ваших мыслях, чувствах и переживаниях. "
+                                "Эти вопросы помогут нам лучше понять эмоциональное благополучие и то, как люди переживают свою повседневную жизнь.\n\n"
+                                "💡 <b>Чего ожидать?</b>\n"
+                                "  -<b>14 вопросов</b> о ваших эмоциях, настроении и взаимодействии с другими.\n"
+                                "  -Вам необходимо отвечать на вопросы, записывая <i><b>голосовые сообщения</b></i>.\n"
+                                "  -Нет правильных или неправильных ответов – просто делитесь тем, чем что вам кажется правильным.\n"
+                                "  -Ваши ответы <b>конфиденциальны</b> и используются только для научных исследований.\n\n"
+                                "⚠️ <b>Важно:</b> Этот бот <b>не предоставляет психологической помощи</b>. Если вам не комфортно, вы можете остановиться в любой момент. "
+                                "Если вам нужна поддержка, обратитесь к профессионалу.\n\n"
+                                "👉 Если вы готовы, отправьте любое голосовое сообщение, чтобы перейти к вопросам!",
+    },
+
+
+    'end_main_survey_message': {
+        'en': 'Thank you for participating in the survey!',
+        'de': 'Vielen Dank für die Teilnahme an der Umfrage!',
+        'ru': 'Спасибо за участие в опросе!'
+    },
+    'voice_out_of_survey': {
+        'en': 'You are out of the survey, where should answer with voice messages. Please, type /start to start the survey again.',
+        'de': 'Sie sind aus der Umfrage, in der Sie mit Sprachnachrichten antworten sollten. Bitte geben Sie /start ein, um die Umfrage erneut zu starten.',
+        'ru': 'Вы вышли из опроса, где должны отвечать голосовыми сообщениями. Пожалуйста, введите /start, чтобы начать опрос снова.'
+    },
+    'voice_recieved': {
+        'en':'Your voice message has been received. If you want to send another answer, please do so. Otherwise, you can proceed to the next or previous question.',
+        'de':'Ihre Sprachnachricht wurde empfangen. Wenn Sie eine weitere Antwort senden möchten, tun Sie dies bitte. Andernfalls können Sie mit der nächsten oder vorherigen Frage fortfahren.',
+        'ru':'Ваше голосовое сообщение получено. Если вы хотите отправить другой ответ, пожалуйста, сделайте это. В противном случае, вы можете перейти к следующему или предыдущему вопросу.'
+    },
+
+    "share_bot_button": {
+        'en': 'Share the bot',
+        'de': 'Teilen Sie den Bot',
+        'ru': 'Поделиться ботом'
+    },
+    "share_bot_text": {
+        'en': 'I invite you to participate in a scientific study on mental health. Use this bot to help scientists and contribute to science!',
+        'de': 'Ich lade ein, an einer wissenschaftlichen Studie über psychische Gesundheit teilzunehmen. Verwende diesen Bot, um Wissenschaftlern zu helfen und einen Beitrag zur Wissenschaft zu leisten!',
+       'ru': 'Приглашаю принять участие в научном исследовании о ментальном здоровье. Используй этого бота, чтобы помочь ученым и внести свой вклад в науку!'
+    }
+
+
+}
