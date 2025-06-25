df7b17370931cbc1552c7c27ab415f8c84fdb2d6
Danila Mamontov <goomenny@gmail.com>
2025-06-25 23:28:41 +0200

    Fix SqliteStateStorage to ignore bot_id param

--- utils/db_state_storage.py
+++ utils/db_state_storage.py
@@ -6,25 +6,25 @@ class SqliteStateStorage:
     def __init__(self):
         self._data = {}
 
-    def get_state(self, user_id, chat_id=None):
+    def get_state(self, user_id, chat_id=None, **kwargs):
         session = load_session(user_id)
         return session.get("fsm_state") if session else None
 
-    def set_state(self, user_id, state, chat_id=None):
+    def set_state(self, user_id, state, chat_id=None, **kwargs):
         session = load_session(user_id) or {}
         session["fsm_state"] = str(state) if state is not None else None
         save_session(user_id, session)
 
-    def delete_state(self, user_id, chat_id=None):
+    def delete_state(self, user_id, chat_id=None, **kwargs):
         session = load_session(user_id) or {}
         session["fsm_state"] = None
         save_session(user_id, session)
 
-    def get_data(self, user_id, chat_id=None):
+    def get_data(self, user_id, chat_id=None, **kwargs):
         return self._data.setdefault((user_id, chat_id), {})
 
-    def set_data(self, user_id, chat_id=None, data=None):
+    def set_data(self, user_id, chat_id=None, data=None, **kwargs):
         self._data[(user_id, chat_id)] = data or {}
 
-    def reset_data(self, user_id, chat_id=None):
+    def reset_data(self, user_id, chat_id=None, **kwargs):
         self._data.pop((user_id, chat_id), None)
